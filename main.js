bb0d9d34e03d360684db507d523614cb0398f662
Martin Federico Moran <martinfmoran@icloud.com>
2022-05-27 16:38:48 -0300

    modificacion formulario contacto  y agregue js.main

---
+++ main.js
@@ -0,0 +1,19 @@
+function Init()
+{
+    formSubmit();
+}
+
+function formSubmit()
+{
+    const nodeBtn = document.getElementById("btnForm");
+
+    nodeBtn.addEventListener("click", ()=> Submit());
+}
+
+function Submit()
+{
+    if(inputs.value!=="")
+    {
+        
+    }    
+}
